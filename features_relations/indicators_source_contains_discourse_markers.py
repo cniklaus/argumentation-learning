35f56d93cbc52bc8625a7131bc8d74c94613f361
cniklaus <niklaus.christina@googlemail.com>
2020-08-26 12:42:57 +0200

    Add files via upload

---
+++ features_relations/indicators_source_contains_discourse_markers.py
@@ -0,0 +1,57 @@
+import spacy
+
+nlp = spacy.load('de_core_news_sm')
+
+causal_markers = ["da", "denn", "weil"]
+consecutive_markers = ["also", "dadurch", "daher", "darum", "deshalb", "deswegen", "drum"]
+adversative_markers = ["anstatt", "anderenfalls", "andererseits", "einerseits", "hingegen", "jedoch", "sondern", "statt", "stattdessen", "vielmehr", "wiederum", "zum einen", "zum anderen"]
+concessive_markers = ["allerdings", "dennoch", "gleichwohl", "nichtsdestotrotz", "nichtsdestoweniger", "obschon", "obwohl", "trotzdem", "wenngleich", "wobei", "zwar"]
+conditional_markers = ["falls", "ob", "sofern", "wenn"]
+
+argumentative_discourse_markers = causal_markers + consecutive_markers + adversative_markers + concessive_markers + conditional_markers
+
+
+def contains_causal_markers(sentence):
+    for token in sentence:
+        if token.text.lower() in causal_markers:
+            return True
+    return False
+
+
+def contains_consecutive_markers(sentence):
+    for token in sentence:
+        if token.text.lower() in consecutive_markers:
+            return True
+    return False
+
+
+def contains_adversative_markers(sentence):
+    for token in sentence:
+        if token.text.lower() in adversative_markers:
+            return True
+    return False
+
+
+def contains_concessive_markers(sentence):
+    for token in sentence:
+        if token.text.lower() in concessive_markers:
+            return True
+    return False
+
+
+def contains_conditional_markers(sentence):
+    for token in sentence:
+        if token.text.lower() in conditional_markers:
+            return True
+    return False
+
+
+def contains_argumentative_markers(sentence):
+    argumentative_discourse_markers = causal_markers + consecutive_markers + adversative_markers + concessive_markers + conditional_markers
+    for token in nlp(sentence):
+        if token.text.lower() in argumentative_discourse_markers:
+            return True
+    return False
+
+
+# print(contains_argumentative_markers("Er ging nach Hause, da es spät war."))
